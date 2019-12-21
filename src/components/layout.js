a39d1e56355938a44d2be979f58bd968db8948f2
Amber <amber.j.nola@gmail.com>
2019-12-20 21:55:59 -0600

    saving changes and pages added to index

--- src/components/layout.js
+++ src/components/layout.js
@@ -36,7 +36,7 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Designed and built by Amber with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
---
+++ src/pages/portfolio.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Portfolio = () => (
+  <section id="portfolio">
+    <SEO title="Page two" />
+    <h1>Hi from the About page</h1>
+    <p>Welcome to About ME</p>
+  </section>
+
+)
+
+export default Portfolio
