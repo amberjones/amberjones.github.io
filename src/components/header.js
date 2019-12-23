3c2a001a63f5c52d420993ec11fb27880fe3f034
Amber <amber.j.nola@gmail.com>
2019-12-23 01:16:48 -0600

    switched sides of icons and nav links

--- src/components/header.js
+++ src/components/header.js
@@ -4,13 +4,14 @@ import React from "react"
 import logo from "../images/ajIcon.svg"
 
 const Header = ({ siteTitle }) => (
-  <header id="header" style={{
-    margin: `auto`,
-    padding: `0, 40`,
-  }}
-  >
+  <header id="header">
     <nav class="nav" style={{marginLeft: 30, marginRight: 30}}>
-      <div class="nav_Styledlogo" style={{paddingTop: 20 }}>
+      <div class="nav_Links">
+        <Link to="#about"> About</Link>
+        <Link to="#portfolio"> Portfolio</Link>
+        <Link to="#contact"> Contact </Link>
+      </div>
+    <div class="nav_Styledlogo" style={{marginTop: 10}}>
         <a href="https://dev.to/amberjones">
           <img src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg" alt="AmberJ's DEV Profile" height="40" width="40"></img>
         </a>
@@ -20,16 +21,9 @@ const Header = ({ siteTitle }) => (
         <a href="https://github.com/amberjones">
           <img src="https://i.imgur.com/2o38lKB.png" alt="AmberJ's Github Profile" height="40" width="40"></img>
         </a>
-      </div>
-      <div class="nav_Links">
-      <Link to="#about"> About</Link>
-      <Link to="#portfolio"> Portfolio</Link>
-      <Link to="#contact"> Contact </Link>
-      </div>
+    </div>
     </nav>
-
   </header>
-  
 )
 
 Header.propTypes = {
